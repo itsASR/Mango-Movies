e3d48ba6a02926a7fb21abb1b7009b101846420c
itsASR <68419561+itsASR@users.noreply.github.com>
2023-10-29 13:47:49 +0530

    Update Onlymoviespage.jsx

--- src/subpage/Onlymoviespage.jsx
+++ src/subpage/Onlymoviespage.jsx
@@ -4,7 +4,7 @@ import { consta } from '../App'
 import '../Moviepage.css'
 import Footer from '../Footer'
 import mov from '../movie-background.jpg'
-import './TVshowsmain.css'
+import './Tvshowsmain.css'
 
 
 function Onlymoviespage() {
